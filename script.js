e4d1dcf4ca1698424d4e56469ce78e8e47ff25b3
Doligame <Dolgaming321@gmail.com>
2025-05-24 23:19:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+function increase() {
+    count++;
+    document.getElementById("counter").innerText = count;
+}
+
+function reset() {
+    count = 0;
+    document.getElementById("counter").innerText = count;
+}
